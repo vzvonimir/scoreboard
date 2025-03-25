ab0837a5c62333393c04de818c6c0e07864daee5
vzvonimir <zvonimir.vistica99@gmail.com>
2025-03-25 20:43:06 +0100

    startMatch edge cases

--- src/main/java/com/scoreboard/ScoreBoard.java
+++ src/main/java/com/scoreboard/ScoreBoard.java
@@ -7,10 +7,30 @@ public class ScoreBoard {
     private final List<Match> matches = new ArrayList<>();
 
     public void startMatch(String homeTeam, String awayTeam){
+        if(homeTeam == null || awayTeam == null || homeTeam.isBlank() || awayTeam.isBlank()){
+            throw new IllegalArgumentException("Team names can't be null or empty!");
+        }
+        if(homeTeam.equalsIgnoreCase(awayTeam)){
+            throw new IllegalArgumentException("Team cannot play against itself!");
+        }
+        if(matchAlreadyExists(homeTeam, awayTeam)){
+            throw new IllegalArgumentException("This match is already in progress!");
+        }
+
         matches.add(new Match(homeTeam, awayTeam));
     }
 
     public List<Match> getMatches(){
         return matches;
     }
+
+    private boolean matchAlreadyExists(String homeTeam, String awayTeam){
+        for(Match match : matches){
+            if(match.getHomeTeam().equalsIgnoreCase(homeTeam) && match.getAwayTeam().equalsIgnoreCase(awayTeam)){
+                return true;
+            }
+        }
+        return false;
+    }
+
 }
--- src/test/java/com/scoreboard/ScoreBoardTest.java
+++ src/test/java/com/scoreboard/ScoreBoardTest.java
@@ -1,7 +1,6 @@
 package com.scoreboard;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.Assertions.*;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -22,5 +21,46 @@ public class ScoreBoardTest {
         assertEquals("Brazil", match.getAwayTeam());
         assertEquals(0, match.getHomeScore());
         assertEquals(0, match.getAwayScore());
+    }
+
+    @Test
+    void startMatch_shouldThrowIfTeamsAreTheSame(){
+        ScoreBoard scoreBoard = new ScoreBoard();
+
+        assertThrows(IllegalArgumentException.class, () -> {
+           scoreBoard.startMatch("Spain", "Spain");
+        });
+    }
+
+    @Test
+    void startMatch_shouldThrowIfTeamNamesAreNullOrEmpty(){
+        ScoreBoard scoreBoard = new ScoreBoard();
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            scoreBoard.startMatch(null, "Brazil");
+        });
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            scoreBoard.startMatch("Spain", null);
+        });
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            scoreBoard.startMatch("", "Brazil");
+        });
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            scoreBoard.startMatch("Spain", "");
+        });
+    }
+
+    @Test
+    void startMatch_shouldThrowIfMatchAlreadyExists(){
+        ScoreBoard scoreBoard = new ScoreBoard();
+
+        scoreBoard.startMatch("Spain", "Brazil");
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            scoreBoard.startMatch("Spain", "Brazil");
+        });
     }
 }
